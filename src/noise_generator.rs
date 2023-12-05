e50819cbad4250918da2be7ef39c17077f5d1e7f
jacudibu <jacudibu@gmail.com>
2023-12-05 10:41:58 +0100

    Turns out noise is in range [-1, 1] and not [0, 1]

--- src/noise_generator.rs
+++ src/noise_generator.rs
@@ -15,6 +15,10 @@ impl NoiseGenerator {
         }
     }
 
+    fn get_01(&self, x: f64, y: f64) -> f32 {
+        ((self.generator.get([x * SCALE, y * SCALE]) + 1.0) * 0.5) as f32
+    }
+
     fn get(&self, chunk_pos: IVec2, tile_pos_x: u32, tile_pos_y: u32) -> f32 {
         let x = (chunk_pos.x as f64 * crate::game_map::CHUNK_SIZE.x as f64) + tile_pos_x as f64;
         let y = (chunk_pos.y as f64 * crate::game_map::CHUNK_SIZE.y as f64) + tile_pos_y as f64;
@@ -27,9 +31,10 @@ impl NoiseGenerator {
         let y = (chunk_pos.y as f64 * crate::game_map::CHUNK_SIZE.y as f64) + tile_pos_y as f64;
 
         Color::rgb(
-            self.generator.get([x * SCALE, y * SCALE, 0.0]) as f32,
-            self.generator.get([x * SCALE, y * SCALE, 1.0]) as f32,
-            self.generator.get([x * SCALE, y * SCALE, 2.0]) as f32,
+            0.0,
+            //self.get_01(x, y, 0.0),
+            self.get_01(x, y),
+            0.0, //self.get_01(x, y, 2.0),
         )
     }
 }
